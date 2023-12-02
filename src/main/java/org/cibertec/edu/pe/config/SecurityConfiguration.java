d76226c828d19338b6ced0cc5c6c80cdc162dbb2
guillermo-penadillo <guillermopc51@gmail.com>
2023-12-02 14:19:47 -0500

    commit con bot

--- src/main/java/org/cibertec/edu/pe/config/SecurityConfiguration.java
+++ src/main/java/org/cibertec/edu/pe/config/SecurityConfiguration.java
@@ -1,14 +1,18 @@
 package org.cibertec.edu.pe.config;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.cibertec.edu.pe.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -47,7 +51,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
             .antMatchers(resources).permitAll()
-            .antMatchers("/", "/login","/registro").permitAll()
+            .antMatchers("/", "/login", "/registro").permitAll()
             .antMatchers("/empleado/**").hasRole("MANTENEDOR")
             .antMatchers("/cliente/**").hasRole("CLIENTE")
             .antMatchers("/admin/**").hasRole("ADMIN")
@@ -65,5 +69,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .permitAll()
             .logoutSuccessUrl("/login?logout");
     }
+
 
 }
--- src/main/resources/bd/data.sql
+++ src/main/resources/bd/data.sql
@@ -1,11 +1,8 @@
-INSERT IGNORE INTO tb_categorias (nombre_cat, desc_cat) VALUES ('Antivirus', 'Software de seguridad para proteger tu computadora');
-INSERT IGNORE INTO tb_categorias (nombre_cat, desc_cat) VALUES ('Diseño Gráfico', 'Herramientas de diseño para profesionales creativos');
-INSERT IGNORE INTO tb_categorias (nombre_cat, desc_cat) VALUES ('Productividad', 'Software de oficina para aumentar la eficiencia laboral');
-INSERT IGNORE INTO tb_categorias (nombre_cat, desc_cat) VALUES ('Desarrollo Web', 'Herramientas para crear aplicaciones web y sitios web');
-INSERT IGNORE INTO tb_categorias (nombre_cat, desc_cat) VALUES ('Sistemas Operativos', 'Sistemas operativos para computadoras');
-
-INSERT IGNORE INTO tb_tipos_empleado (nombre_tipo, desc_tipo) VALUES ('Administrador', 'Encargado de la gestión y supervisión general');
-INSERT IGNORE INTO tb_tipos_empleado (nombre_tipo, desc_tipo) VALUES ('Mantenedor', 'Encargado del mantenimiento de productos y categorías');
+INSERT IGNORE INTO tb_categorias (id_cat,nombre_cat, desc_cat) VALUES (1,'Antivirus', 'Software de seguridad para proteger tu computadora');
+INSERT IGNORE INTO tb_categorias (id_cat,nombre_cat, desc_cat) VALUES (2,'Diseño Gráfico', 'Herramientas de diseño para profesionales creativos');
+INSERT IGNORE INTO tb_categorias (id_cat,nombre_cat, desc_cat) VALUES (3,'Productividad', 'Software de oficina para aumentar la eficiencia laboral');
+INSERT IGNORE INTO tb_categorias (id_cat,nombre_cat, desc_cat) VALUES (4,'Desarrollo Web', 'Herramientas para crear aplicaciones web y sitios web');
+INSERT IGNORE INTO tb_categorias (id_cat,nombre_cat, desc_cat) VALUES (5,'Sistemas Operativos', 'Sistemas operativos para computadoras');
 
 INSERT IGNORE INTO rol (id,nombre) VALUES (1, 'ROLE_CLIENTE');
 INSERT IGNORE INTO rol (id,nombre) VALUES (2, 'ROLE_ADMIN');
